8a5bc654c6afe80cf98734bc01edd1a45651cee3
Saeedx303 <130200805+Saeedx303@users.noreply.github.com>
2024-02-23 04:24:06 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,24 +11,24 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //owmner v card
-global.ytname = "YT: S4 Salman YT" //ur yt chanel name
-global.socialm = "GitHub: salmanytofficial" //ur github or insta name
+global.ytname = "YT: BABLI YT" //ur yt chanel name
+global.socialm = "GitHub: Saeedx303" //ur github or insta name
 global.location = "Pakistan, Lahore, Sabzazar" //ur location
 
 //new
-global.botname = 'X-BOT-MD' //ur bot name
-global.ownernumber = '923184070915' //ur owner number
-global.ownername = 'Salman Ahmad' //ur owner name
-global.websitex = "https://youtu.com/@s4salmanyt"
+global.botname = 'BABLI-BOT-MD' //ur bot name
+global.ownernumber = ['923361501800', 'Saeed', true], ['923477008403', 'Nisar', true], ['923137183974', 'Nham Noys', true]  //ur owner number
+global.ownername = 'BABLIxBABLI' //ur owner name
+global.websitex = "https://youtu.com/Babli"
 global.wagc = "https://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e"
 global.themeemoji = '🪀'
 global.wm = "X Bot Inc."
-global.botscript = 'https://github.com/salmanytofficial/XLICON-V2-MD' //script link
-global.packname = "𝙂𝙊𝙅𝙊 𝙆𝙐𝙉"
-global.author = "ᗰᗩᗪE ᗷY 𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿"
-global.creator = "923184070915@s.whatsapp.net"
+global.botscript = 'https://github.com/Saeedx303' //script link
+global.packname = "Small|Babli"
+global.author = "ᗰᗩᗪE ᗷY BablixBablli"
+global.creator = "9233361501800@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["923184070915"] // Premium User
+global.premium = ['923361501800', '923137183974', '923477008403'] // Premium User
 global.hituet = 0
 
 //bot sett
@@ -36,18 +36,18 @@ global.typemenu = 'v4' // menu type 'v1' => 'v8'
 global.typereply = 'v2' // reply type 'v1' => 'v3'
 global.autoblocknumber = '212' //set autoblock country code
 global.antiforeignnumber = '212' //set anti foreign number country code
-global.welcome = false //welcome/left in groups
+global.welcome = true //welcome/left in groups
 global.anticall = false //bot blocks user when called
-global.autoswview = false //auto status/story view
+global.autoswview = true //auto status/story view
 global.adminevent = false //show promote/demote message
-global.groupevent = false //show update messages in group chat
+global.groupevent = true //show update messages in group chat
 //msg
 global.mess = {
 	limit: 'Your limit is up!',
-	nsfw: 'Nsfw is disabled in this group, Please tell the admin to enable',
+	nsfw: 'Nsfw is disabled in this group, Please tell the BABLI to enable',
     done: 'Done✓',
     error: 'Error!',
-    success: 'Here you go!'
+    success: 'Here you go Babli!'
 }
 //thumbnail
 global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
